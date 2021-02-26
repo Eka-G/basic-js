45f34aa208bfd92e8e70d38957a40cf24235263e
Eka-G <ekaglazkovaa@yandex.ru>
2021-02-26 16:45:40 +0300

    feat: compleate count-cats, dream-team, hanoi-tower, transform-array, what-season

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let res = {
+    turns: 1,
+    seconds: 1,
+  };
+
+  for (i = 1; i < disksNumber; i++) {
+    res.turns = res.turns * 2 + 1; //каждый последующий диск добавлет *2+1 шагов
+  }
+
+  res.seconds = Math.floor(res.turns * 60 * 60 / turnsSpeed);
+  return res;
 };
