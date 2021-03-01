3c2afa036a25dbe4c073d22383713cd81075309f
Eka-G <ekaglazkovaa@yandex.ru>
2021-03-01 16:36:30 +0300

    fix: extended-repeater correctly works with no separator

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -5,12 +5,13 @@ module.exports = function repeater(str, options) {
     value = str;
 
   if (options["separator"] === undefined) options["separator"] = "+";
+  if (options["additionSeparator"] === undefined) options["additionSeparator"] = "|";
   if (options["repeatTimes"] === undefined) options["repeatTimes"] = 1;
   if (options["additionRepeatTimes"] === undefined) options["additionRepeatTimes"] = 1;
 
   if (options["addition"] !== undefined) {
     for (let i = 0; i < options["additionRepeatTimes"]; i++) {
-      (i === options["additionRepeatTimes"] - 1 || !options["additionSeparator"]) ?
+      i === options["additionRepeatTimes"] - 1 ?
         value += options["addition"] : value += options["addition"] + options["additionSeparator"];
     }
   }
